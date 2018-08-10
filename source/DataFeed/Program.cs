5a111bdd6dfa88cb398c7fc1e3f2d6e530f960c6
MohitMehta1986 <38661250+MohitMehta1986@users.noreply.github.com>
2018-08-10 06:04:42 +0100

    Update Program.cs

--- source/DataFeed/Program.cs
+++ source/DataFeed/Program.cs
@@ -6,7 +6,10 @@ namespace DataFeed
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             var hostname = "172.17.0.6,1433";
+            var password = "MyStrongPassword1234";
+            var connString = $"Data Source={hostname};Initial Catalog=master;User ID=sa;Password={password};";
+            Console.WriteLine("connection string {0}", connString);
         }
     }
 }
