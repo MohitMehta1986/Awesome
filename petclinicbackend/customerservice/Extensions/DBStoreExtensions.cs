1618afb394720ae60d609d4a1e1af6c82c37289c
Golyan <Priyanka.Golyan@gartner.com>
2019-04-17 22:39:19 +0530

    Change titles and styling on homepage

---
+++ petclinicbackend/customerservice/Extensions/DBStoreExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Cloud.Datastore.V1;
+using petclinicmicroservice.AngularWebApi.Models;
+
+namespace petclinicmicroservice.Extensions
+{
+	public static class DatastoreBookStoreExtensionMethods
+	{
+		/// <summary>
+		/// Make a datastore key given a book's id.
+		/// </summary>
+		/// <param name="id">A book's id.</param>
+		/// <returns>A datastore key.</returns>
+		public static Key ToKey(this string id) =>
+			new Key().WithElement("owner", id);
+
+		/// <summary>
+		/// Make a book id given a datastore key.
+		/// </summary>
+		/// <param name="key">A datastore key</param>
+		/// <returns>A book id.</returns>
+		public static long ToId(this Key key) => key.Path.First().Id;
+
+		/// <summary>
+		/// Create a datastore entity with the same values as book.
+		/// </summary>
+		/// <param name="book">The book to store in datastore.</param>
+		/// <returns>A datastore entity.</returns>
+		/// [START toentity]
+		public static Entity ToEntity(this Owner owner) => new Entity()
+		{
+			/* id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  address VARCHAR(255),
+  city VARCHAR(80),
+  telephone VARCHAR(20),
+  INDEX(last_name)*/
+
+			Key = owner.id.ToString().ToKey(),
+			["first_name"] = owner.firstName,
+			["last_name"] = owner.lastName,
+			["address"] = owner.address,
+			["city"] = owner.city,
+			["telephone"] = owner.telephone
+			,};
+		// [END toentity]
+
+		/// <summary>
+		/// Unpack a book from a datastore entity.
+		/// </summary>
+		/// <param name="entity">An entity retrieved from datastore.</param>
+		/// <returns>A book.</returns>
+		public static Owner ToOwner(this Entity entity) => new Owner()
+		{
+			id = unchecked((int)entity.Key.Path.First().Id),
+			firstName = (string)entity["first_name"],
+			lastName = (string)entity["lastname"],
+			address = (string)entity["address"],
+			city = (string)entity["city"],
+			telephone = (string)entity["telephone"],
+			
+		};
+	}
+}
---
+++ petclinicbackend/customerservice/Helper/OwnerGCPRepository.cs
@@ -0,0 +1,47 @@
+﻿using Google.Cloud.Datastore.V1;
+using petclinicmicroservice.AngularWebApi.Models;
+using petclinicmicroservice.Extensions;
+using petclinicmicroservice.Interfaces;
+using System;
+using System.Linq;
+
+namespace petclinicmicroservice.Helper
+{
+	public class OwnerGcpRepository:IOwnerRepository
+    {
+	    private readonly string projectId;
+	    private readonly DatastoreDb db;
+		public OwnerGcpRepository(string projectId)
+		{
+			this.projectId = projectId;
+			db=DatastoreDb.Create(projectId);
+		}
+	    public void CreateOwner(Owner owner)
+	    {
+		    var entity = owner.ToEntity();
+		    db.Insert(new[] {entity});
+	    }
+
+	    public Owner FindOwner(string ownerid)
+	    {
+			return db.Lookup(ownerid.ToKey())?.ToOwner();
+		}
+
+	    public void DeleteOwner(string ownerId)
+	    {
+		    db.Delete(ownerId.ToKey());
+	    }
+
+	    public Owner[] FindAll()
+	    {
+			var query = new Query("owner") { Limit = 10 };
+		    var results = db.RunQuery(query);
+		    return results.Entities.Select(entity => entity.ToOwner()).ToArray();
+	    }
+
+	    public void UpdateOwner(Owner owner)
+	    {
+			db.Update(owner.ToEntity());
+		}
+    }
+}
