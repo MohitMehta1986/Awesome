dfc4aee993edc1e6891586ea8ae120b7e46c8f75
MohitMehta1986 <38661250+MohitMehta1986@users.noreply.github.com>
2019-04-29 12:06:27 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
 dotnet restore petclinicbackend/customerservice/petclinicmicroservice.csproj
-dotnet build --sorce petclinicbackend/customerservice/petclinicmicroservice.csproj -c Release
+dotnet build --source petclinicbackend/customerservice/petclinicmicroservice.csproj -c Release
